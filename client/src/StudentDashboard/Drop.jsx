33ab81e4303173877990ac2ab0e48924a8169110
Atanu789 <atanurandhawa@gmail.com>
2024-04-02 02:04:45 +0530

    "OKEY"

--- client/src/StudentDashboard/Drop.jsx
+++ client/src/StudentDashboard/Drop.jsx
@@ -1,24 +1,19 @@
-import { Fragment, useEffect, useState } from 'react'
+import { Fragment, useState } from 'react'
 import { Menu, Transition } from '@headlessui/react'
 import { ChevronDownIcon } from '@heroicons/react/20/solid'
 import Sidebar from './Sidebar'
 
-function classNames(...classes) {
-  return classes.filter(Boolean).join(' ')
-}
-
 export default function Example2() {
   const [isOpen, setIsOpen] = useState(false);
-   
 
   return (
-    <Menu as="div" className="relative inline-block text-left">
+    <Menu as="div" className="relative inline-block text-left z-10">
       <div>
         <Menu.Button
           className="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-#115cd4 px-3 py-2  text-sm font-semibold text-white shadow-sm ring-1 ring-inset ring-#115cd4 hover:bg-gray-800 ml-0"
           onClick={() => setIsOpen(!isOpen)}
         >
-         Menu
+          Menu
           <ChevronDownIcon className="-mr-1 h-5 w-5 text-white" aria-hidden="true" />
         </Menu.Button>
       </div>
@@ -33,7 +28,7 @@ export default function Example2() {
         leaveFrom="opacity-100 translate-y-0"
         leaveTo="opacity-0 translate-y-[-30%]"
       >
-        <Menu.Items className="absolute left-0 z-10 mt-6">
+        <Menu.Items className="absolute left-0 z-10 mt-6"> {/* Updated z-index to 10 */}
           <div className="py-1">
             <Menu.Item>
              <Sidebar/>
