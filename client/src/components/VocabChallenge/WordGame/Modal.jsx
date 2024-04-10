d4f28604d6e397bc1cefc0edcb5e61a6f6c09700
bishnudev35 <bishnudevsardar35@gmail.com>
2024-04-10 08:34:03 +0530

    "word_game_is_ok"

--- client/src/components/VocabChallenge/WordGame/Modal.jsx
+++ client/src/components/VocabChallenge/WordGame/Modal.jsx
@@ -15,11 +15,11 @@ function Modal({ gameStatus, handleGameReset, solution }, ref) {
   if (!openModal) return null;
 
   return (
-    <div className="backdrop">
+    <div className="backdrop" style={{  top:"500px", bottom:"-700px"}}>
       <div className="modal">
         <p>{gameStatus}</p>
         <p>The word was: {solution.toUpperCase()}</p>
-        <button onClick={onClose}> Play Again </button>
+        <button onClick={onClose} className=" top-10 bg-[#77a832] rounded-md  text-black"> Play Again </button>
       </div>
     </div>
   );
